822e0621a40ed89483ceebb2fb93835a8d8d868c
jmvan <jmvanw@gmail.com>
2017-10-23 16:49:45 -0700

    Triggers created
    Changing status from being checked out to being checked in will now move
    record to the history table and delete it from the loans table.

--- DbTableCreation.sql
+++ DbTableCreation.sql
@@ -50,7 +50,7 @@ SELECT * FROM BOOKS
 
 CREATE TABLE BOOK_COPIES
 (
-BookCode INT Primary Key NOT NULL,
+BookCode BIGINT Primary Key NOT NULL,
 BookID INT Foreign Key REFERENCES BOOKS(BookID) NOT NULL,
 BranchID INT Foreign Key REFERENCES LIBRARY_BRANCH(BranchID) NOT NULL
 );
@@ -59,18 +59,18 @@ SELECT * FROM BOOK_COPIES
 
 CREATE TABLE BOOK_LOANS
 (
-BookCode INT Foreign Key REFERENCES BOOK_COPIES(BookCode) NOT NULL,
+BookCode BIGINT Foreign Key REFERENCES BOOK_COPIES(BookCode) NOT NULL,
 CardNo INT Foreign Key REFERENCES BORROWER(CardNo) NOT NULL,
 [Status] BIT  NOT NULL,
 DateOut DATE NOT NULL
 --DateDue DATE NOT 
 
 );
-
+SELECT * FROM BOOK_LOANS
 
 CREATE TABLE BOOK_LOAN_HISTORY
 (
-BookCode INT Foreign Key REFERENCES BOOK_COPIES(BookCode) NOT NULL,
+BookCode BIGINT Foreign Key REFERENCES BOOK_COPIES(BookCode) NOT NULL,
 CardNo INT Foreign Key REFERENCES BORROWER(CardNo) NOT NULL,
 [Status] BIT  NOT NULL,
 DateOut DATE NOT NULL,
@@ -78,17 +78,69 @@ DateDue DATE NOT NULL
 
 );
 
--- When Status changes from 1 to 0, update both BOOK_LOANS and BOOK_LOANS_HISTORY, inserting into BOOK_LOANS_HISTORY and deleting BOOK_LOANS row
+-- When Status changes from 1 to 0, update both BOOK_LOANS and BOOK_LOANS_HISTORY, 
+--inserting into BOOK_LOANS_HISTORY and deleting BOOK_LOANS row
 
---CREATE TRIGGER checkedIn 
---ON BOOK_LOANS FOR 
---UPDATE AS 
---BEGIN
---	IF UPDATE ([Status])
---	BEGIN
---		INSERT INTO BOOK_LOAN_HISTORY
---		SELECT * FROM BOOK_LOANS
---		WHERE [Status] = 0
+CREATE TRIGGER checkedIn 
+ON BOOK_LOANS FOR 
+UPDATE AS 
+BEGIN
+	IF UPDATE ([Status])
+	BEGIN
+		INSERT INTO BOOK_LOAN_HISTORY
+		SELECT * FROM BOOK_LOANS
+		WHERE [Status] = 0
+	END
+END
+
+CREATE TRIGGER checkedInPrt2 
+ON BOOK_LOANS FOR 
+UPDATE AS 
+BEGIN
+	IF UPDATE ([Status])
+	BEGIN
+		DELETE BOOK_LOANS
+		FROM BOOK_LOANS
+		INNER JOIN BOOK_LOAN_HISTORY 
+		ON BOOK_LOANS.BookCode = BOOK_LOAN_HISTORY.BookCode
+		
+	END
+END
+
+
+SELECT * FROM BOOK_LOANS
+SELECT * FROM BOOK_LOAN_HISTORY
+
+--create trigger bookStatus_trigger
+--on BOOK_LOANS
+--after UPDATE, INSERT, DELETE
+--as
+--declare @BookCode int,@user varchar(20), @activity varchar(20);
+--if exists(SELECT * from inserted) and exists (SELECT * from deleted)
+--begin
+--    SET @activity = 'UPDATE';
+--    SET @user = SYSTEM_USER;
+--    SELECT @EmpID = EmployeeID from inserted i;
+--    INSERT into Emp_Audit(EmpID,Activity, DoneBy) values (@EmpID,@activity,@user);
+--end
+
+--If exists (Select * from inserted) and not exists(Select * from deleted)
+--begin
+--    SET @activity = 'INSERT';
+--    SET @user = SYSTEM_USER;
+--    SELECT @EmpID = EmployeeID from inserted i;
+--    INSERT into Emp_Audit(EmpID,Activity, DoneBy) values(@EmpID,@activity,@user);
+--end
+
+--If exists(select * from deleted) and not exists(Select * from inserted)
+--begin 
+--    SET @activity = 'DELETE';
+--    SET @user = SYSTEM_USER;
+--    SELECT @EmpID = EmployeeID from deleted i;
+--    INSERT into Emp_Audit(EmpID,Activity, DoneBy) values(@EmpID,@activity,@user);
+--end
+
+
 
 
 
@@ -102,11 +154,12 @@ AuthorName varchar(150) Null
 
 SELECT * FROM BOOK_AUTHORS
 
---Trigger to add record on update of BOOK_COPIES, and add record on update of [status]=0 in BOOK_LOANS and remove record on update of [status]=1 in BOOK_LOANS
+--Trigger to add record on update of BOOK_COPIES, and add record on update of 
+--[status]=0 in BOOK_LOANS and remove record on update of [status]=1 in BOOK_LOANS
 
 CREATE TABLE BRANCH_COPIES
 (
 BranchID INT Foreign Key REFERENCES LIBRARY_BRANCH(BranchID) NOT NULL,
-BookCode INT Foreign Key REFERENCES BOOK_COPIES(BookCode) NOT NULL
+BookCode BIGINT Foreign Key REFERENCES BOOK_COPIES(BookCode) NOT NULL
 );
 
